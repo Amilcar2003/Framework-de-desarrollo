01239022720f97028ab32053f3790889cb2622c0
amilc <amilc@LAPTOP-L9O0VFRL>
2020-06-14 16:13:17 -0600

    Inclusion de Framework de desarrollo

---
+++ FrameworkMVC/Controlador/Home.Controller.php
@@ -0,0 +1,23 @@
+<?php
+  /**
+   * Clase home que permite la primera carga del sistema
+   */
+  class Home
+  {
+
+    function __construct()
+    {
+      // code...
+    }
+
+    public function Inicio()
+    {
+        echo "En clase Home y metodo inicio";
+    }
+	public function Fun2()
+    {
+        echo "En controlador Home y metodo Fun2";
+    }
+  }
+
+ ?>
